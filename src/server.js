5dee301052d541e70820d2b12da6489558183c5e
Felipe <fm3209742@gmail.com>
2023-11-22 16:06:40 -0300

    finish

--- src/server.js
+++ src/server.js
@@ -1,46 +1,53 @@
 const express = require("express");
+const cors = require("cors");
 const { transporter } = require('./utils/sendEmail.js');
-const { object, string, bool } = require("yup");
+const { object, string, ValidationError } = require("yup");
+const { config } = require("dotenv");
 const helmet = require("helmet");
+
+config();
 
 const app = express();
 
+app.use(cors({
+    origin: '*'
+}));
 app.use(express.json());
 app.use(helmet());
 
-app.get('/', function (req, res) {
-    console.log(req.query);
-
-    return res.json({
-        message: "Hello World"
-    });
-});
-
 app.post('/contato', async function (req, res) {
+    console.log(req.body);
+    try {
+        
+        const validator = object({
+            email: string().required("E-mail Obrigatório").email("Verifique o formato do e-mail"),
+            nome: string().required("Nome Obrigatório"),
+            razao: string().required("Razão Obrigatória"),
+            tel: string().max(15),
+            mensagem: string().required('Mensagem obrigatória')
+        });
 
-    // const validator = object({
-    //     email: string().required().email(),
-    // });
+        const mailData = await validator.validate(req.body);
 
-    // transporter.sendMail({
-    //     from: mailData.email,
-    //     to: process.env.EMAIL_USER,
-    //     subject: `${mailData.name} ${mailData.surname} <${mailData.email}>`,
-    //     text: mailData.message,
-    // });
+        await transporter().sendMail({
+            from: mailData.email,
+            to: process.env.EMAIL_USER,
+            subject: `${mailData.nome} <${mailData.email}>`,
+            text: `Nome: ${mailData.nome}\n\nRazão: ${mailData.razao}\n\nMensagem: ${mailData.mensagem}\n\nTelefone: ${mailData.telefone || "Não Informado"}`,
+        });
 
-    // try {
-    //     await validator.validate(req.body);
-    //     return res.json({
-    //         message: "parabens, você digitou o e-mail corretamente"
-    //     });
-    // } catch (error) {
-    //     return res.json({
-    //         erro: "verifique os campos"
-    //     });
-    // }
+        return res.status(200).json({
+            success: true
+        });
 
+    } catch (e) {
+        console.log(e)
+        if (e instanceof ValidationError) {
+            return res.status(400).json({ errors: e.errors });
+        }
 
+        return res.status(500).json({ error: 'internal server error' });
+    }
 });
 
-app.listen(8080, console.log("Servidor está ouvindo na porta 80"));
+app.listen(8080, console.log("Servidor está ouvindo na porta 8080"));
--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -10,7 +10,7 @@ module.exports = {
       secure: true,
       auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASSWORD
+        pass: process.env.EMAIL_PASS
       }
     });
   },
